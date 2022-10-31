395fcef271723eb6ea4638475bfce5f6c04f3caf
LCap <banlazthosow@gmail.com>
2022-10-31 15:42:28 +0200

    created component 'Logo'

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Logo.scss";
+
+export const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="img/logo.svg" alt="logo" />
+    </div>
+  );
+};

923b3efdc841755cf227434e5b3f55762ab3840b
LCap <banlazthosow@gmail.com>
2022-10-31 16:54:24 +0200

    added component 'Footer' to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <main></main>
+      <Footer />
     </div>
   );
 }

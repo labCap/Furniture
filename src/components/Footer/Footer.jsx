2452ef23b87ecafcec9de96b995a7aa06e733747
LCap <banlazthosow@gmail.com>
2022-10-31 16:52:47 +0200

    created component 'Footer'

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Footer.scss";
+
+const footerText = [
+  {
+    title: "Get's your discount",
+    links: [{ text: "Check now !", href: "#" }],
+  },
+  {
+    title: "Our services",
+    links: [
+      { text: "Collection", href: "#" },
+      { text: "How its work", href: "#" },
+      { text: "Estimats", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { text: "Terms of use", href: "#" },
+      { text: "Privacy policy", href: "#" },
+      { text: "Careers", href: "#" },
+    ],
+  },
+  {
+    title: "Contact",
+    links: [
+      {
+        text: "Furniutr2@furniture.com",
+        href: "mailto:Furniutr2@furniture.com",
+      },
+      { text: "(888) 315-500", href: "tel:+(888) 315-500" },
+      { text: "St. VIllage, New York", href: "#" },
+    ],
+  },
+];
+
+const FooterCol = ({ col, title }) => {
+  return (
+    <div className="footer__col">
+      <div className="footer__title">{title}</div>
+      <ul className="footer__list">
+        {col.links.map((data) => (
+          <FooterLink links={data} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+const FooterLink = ({ links }) => {
+  return (
+    <li className="footer__list-item">
+      <a href={links.href} className="footer__list-link">
+        {links.text}
+      </a>
+    </li>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__inner">
+          {footerText.map((data) => (
+            <FooterCol col={data} title={data.title} />
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};

f905cd843e81ceab9870c861c5ddbd3a2852b0d7
LCap <banlazthosow@gmail.com>
2022-10-31 15:43:45 +0200

    added component 'Header' to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header/Header";
+
 function App() {
   return (
-    <div>
-      Hello world!
+    <div className="wrapper">
+      <Header />
     </div>
   );
 }

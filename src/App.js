f94d093cf19bad1b105f9c8ce55c183f2c5ec95d
LCap <banlazthosow@gmail.com>
2022-10-31 19:15:23 +0200

    added component 'About' to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { About } from "./components/About/About";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Intro } from "./components/Intro/Intro";
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <Intro />
+        <About />
       </main>
       <Footer />
     </div>

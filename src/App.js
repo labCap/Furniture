79d98a4a1a2f2f295cec65fb06e0a2d11c07cda9
LCap <banlazthosow@gmail.com>
2022-10-31 22:17:01 +0200

    added section 'TryNow' to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from "./components/Header/Header";
 import { Intro } from "./sections/Intro/Intro";
 import { Benefits } from "./sections/Benefits/Benefits";
 import { Product } from "./sections/Product/Product";
+import { TryNow } from "./sections/TryNow/TryNow";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Benefits />
         <Product />
+        <TryNow />
       </main>
       <Footer />
     </div>

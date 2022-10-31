2ca5e5ebc3a5137d8bb611edf94a68b4e633c892
LCap <banlazthosow@gmail.com>
2022-10-31 17:58:22 +0200

    added component 'Into' to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { Intro } from "./components/Intro/Intro";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <main></main>
+      <main>
+        <Intro />
+      </main>
       <Footer />
     </div>
   );
